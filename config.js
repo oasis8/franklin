edbb50c1e8992a3f2aedefac5687384dda01ff80
jhfeng <23147020+georgefeng@users.noreply.github.com>
2021-08-22 10:11:40 +0000

    feat(gatsby): update url

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
+  url: 'https://oasis8.github.com',
+  pathPrefix: '/franklin',
+  title: 'Blog by Franklin',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
